b2a94ff3e48d47ffaa9206a95c353d5a988eeb79
bbluwbrryes <118600290+bbluwbrryes@users.noreply.github.com>
2023-09-07 12:59:38 +0700

    Update hapus.php

--- latihan9/hapus.php
+++ latihan9/hapus.php
@@ -6,14 +6,14 @@ $id = $_GET["id"];
 if( hapus($id) > 0 ) {
     echo "
         <script>
-            alert('data berhasil ditambahkan!');
+            alert('data berhasil dihapus!');
             document.location.href = 'index.php';
         </script>
     ";
 } else {
     echo "
         <script>
-            alert('data berhasil ditambahkan!');
+            alert('data gagal dihapus!');
             document.location.href = 'index.php';
         </script>
     ";
